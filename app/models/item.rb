d88668236538124499526cd2c506824f3f779000
Pedro Monsalve <codeloopy@gmail.com>
2022-10-16 17:00:54 -0400

    Item model validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_encrypted :item_name, :website, :website_username, :website_watchword, :item_notes
   blind_index :item_name, :website, :website_username, :website_watchword, :item_notes
+
+  validates :item_name, :website, :website_username, :website_watchword, :item_notes, presence: true
 end
